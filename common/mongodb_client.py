0267318d57158f339fe21243f62f8faa274b7af2
rbofficial <58599472+rbofficial@users.noreply.github.com>
2020-01-03 03:32:15 +0530

    Update mongodb_client.py

--- common/mongodb_client.py
+++ common/mongodb_client.py
@@ -4,8 +4,7 @@ MONGO_DB_PORT=27017
 DB_NAME='demo'
 
 client=MongoClient(MONGO_DB_HOST,MONGO_DB_PORT)
-# If there is no previously created database with this name, MongoDB will implicitly create one for the user.
-def get_db(db=DB_NAME):  # here db is a default argument. if nothing is mentioned while calling the func, demo will be called
+def get_db(db=DB_NAME): 
     db=client[db]
     return db
 
